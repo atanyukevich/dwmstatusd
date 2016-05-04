d3063b953585dddbd71dbcc5194c03994b4f9d3a
Alexander Tanyukevich <atanyukevich@gmail.com>
2016-05-04 23:06:03 +0200

    another patches to battery

--- getbat.h
+++ getbat.h
@@ -2,11 +2,11 @@
 #define _getbat_h_
 
 /******************************************************************************/
-#define BAT_DIR "/sys/class/power_supply/BAT1"
+#define BAT_DIR "/sys/class/power_supply/BAT0"
 
-#define NOW_STATE BAT_DIR"/energy_now"
-#define NOW_FULL  BAT_DIR"/energy_full"
-#define NOW_DESIGN BAT_DIR"/energy_full_design"
+#define NOW_STATE BAT_DIR"/charge_now"
+#define NOW_FULL  BAT_DIR"/charge_full"
+#define NOW_DESIGN BAT_DIR"/charge_full_design"
 #define NOW_STATUS BAT_DIR"/status"
 /******************************************************************************/
 
